6f526c254dddd5dfa8415c16c7abf3b37a727540
Naimerith <naimerith89@gmail.com>
2022-03-04 20:31:54 -0500

    Creando componente de home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+//agregamos el decorador 
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+//Esta es la clase que acompaña al decorador
+export class HomeComponent {
+
+}
