4d12b1ab2ef4d01ce02e6c2fca19b5b27ea57832
Naimerith <naimerith89@gmail.com>
2022-03-06 20:30:19 -0500

    terminada la vista del home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChefKitchenComponent } from './chef-kitchen/chef-kitchen.component';
+import { HomeComponent } from './home/home.component';
+import { WaiterTablesComponent } from './waiter-tables/waiter-tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tables', component: WaiterTablesComponent },
+  { path: 'kitchen', component: ChefKitchenComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
