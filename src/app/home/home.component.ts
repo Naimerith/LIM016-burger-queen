79c01369ea730b9f428c98ab5df4188ca58f527c
Naimerith <naimerith89@gmail.com>
2022-03-07 22:07:54 -0500

    Se creo modal para el login del mesero y el chef

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwitchService } from '../services/switch.service';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'my-app';
-  saludo = 'Hola nai'
 
-  constructor() { }
+  modalSwitch: boolean = false;
 
+  //aqui consumimos el servicio 
+  constructor(private modalSS: SwitchService) { }
+
+
+  //esta funcion se inicia apenas se renderice el componente 
+  //apenas inicie el componente necesito escuchar el objeto observable, es decir obtener el valor 
   ngOnInit(): void {
+    this.modalSS.$modal.subscribe((valor) => { //esta funcion es un callback 
+      this.modalSwitch = valor //valor tienen el valor actual del switch -true o false-
+    })
+  }
+
+  openModal() {
+    this.modalSwitch = true; //cuando el modal este encendido abre el modal
   }
 
 }
